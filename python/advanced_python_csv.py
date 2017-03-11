060cb02c3703c511231b009975bce45cad53846f
janniec <jyychang@gmail.com>
2017-03-11 10:51:59 -0600

    FINISHED

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,26 @@
-PLACE YOUR CODE HERE
+# imports
+import pandas as pd
+import numpy as np
+from pprint import pprint
+import csv
+import ast
+
+# get data into frame
+url = "https://raw.githubusercontent.com/thisismetis/dsp/master/python/faculty.csv"
+df = pd.read_csv(url)
+
+# Need to clean up column titles 
+df = df.rename(columns={' degree': 'degree'})
+df = df.rename(columns={' title': 'title'})
+df = df.rename(columns={' email': 'email'})
+df.info()
+
+# writing to csv
+df['email'].to_csv('emails.csv', mode = 'w', index = False)
+
+# verify that it wrote right
+emails_file = pd.read_csv("emails.csv", header=None)
+print emails_file
+
+
+
